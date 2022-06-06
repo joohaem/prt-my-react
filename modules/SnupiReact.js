05bba347f0f03c0e43176d6a6d12aa054ab64d78
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-06-06 16:49:23 +0900

    [refactor] separate file module

--- modules/SnupiReact.js
+++ modules/SnupiReact.js
@@ -1,46 +1,4 @@
-// const element = React.createElement(
-//   "div", --> type
-//   { id: "foo" }, --> props
-//   React.createElement("a", null, "bar"), --> ...children
-//   React.createElement("b")
-// );
-
-// -->
-
-// create TEXT_ELEMENT type for primitive type values (wrapping type, props)
-// for simpler code
-function createTextElement(text) {
-  return {
-    type: "TEXT_ELEMENT",
-    props: {
-      nodeValue: text,
-      children: [],
-    },
-  };
-}
-
-function createElement(type, props, ...children) {
-  return {
-    type,
-    props: {
-      ...props,
-      children: children.map((child) =>
-        typeof child === "object" ? child : createTextElement(child)
-      ),
-    },
-  };
-}
-
-// -->
-
-// const element = {
-//   type: "div",
-//   props: {
-//     children: [a, b],
-//   },
-// };
-
-// -------------------------------------------------------
+import createElement from "./createElement";
 
 function createDom(fiber) {}
 

ae22e221201a8265207fea858d86f38e7d3f4c37
SNUPI <joohaem@daum.net>
2022-06-10 21:04:36 +0900

    [refactor] 모듈 합치기

--- modules/SnupiReact.js
+++ modules/SnupiReact.js
@@ -1,5 +1,46 @@
-import createElement from "./createElement.js";
-import useState from "./useState.js";
+// const element = React.createElement(
+//   "div", --> type
+//   { id: "foo" }, --> props
+//   React.createElement("a", null, "bar"), --> ...children
+//   React.createElement("b")
+// );
+
+// -->
+
+// create TEXT_ELEMENT type for primitive type values (wrapping type, props)
+// for simpler code
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) =>
+        typeof child === "object" ? child : createTextElement(child)
+      ),
+    },
+  };
+}
+
+// -->
+
+// const element = {
+  //   type: "div",
+  //   props: {
+//     children: [a, b],
+//   },
+// };
+
+// -------------------------------------------------------
 
 function createDom(fiber) {
   const dom =
